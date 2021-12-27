239657a296a7a2928f5bb1a876c5009622b29825
Nirmeet <nirmeet.baweja@gmail.com>
2021-12-27 23:13:02 +0000

    added a get request

--- main.go
+++ main.go
@@ -1,10 +1,20 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
+
+type Article struct {
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	Content     string `json:"content`
+}
+
+// global Articles array/slice to store dummy data
+var Articles []Article
 
 func homePage(w http.ResponseWriter, r *http.Request) {
 	// Fprintf used to write a fornatted string to the specified writer
@@ -12,11 +22,26 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Endpoint Hit: homePage")
 }
 
+func returnAllArticles(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Endpoint Hit: returnAllArticles")
+	json.NewEncoder(w).Encode(Articles)
+}
+
 func handleRequests() {
 	http.HandleFunc("/", homePage)
+	// add route to get all the articles
+	http.HandleFunc("/articles", returnAllArticles)
 	log.Fatal((http.ListenAndServe(":3000", nil)))
 }
 
 func main() {
+	Articles = []Article{
+		{Title: "Article 1",
+			Description: "Theme of the article",
+			Content:     "The content of the article goes here."},
+		{Title: "Article 1",
+			Description: "Theme of the article",
+			Content:     "The content of the article goes here."},
+	}
 	handleRequests()
 }
